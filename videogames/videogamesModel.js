c5e9ea34424fd3b613722b285984e45ea9ea7722
kamaboko1 <github65@hotmail.com>
2019-06-28 08:41:02 -0700

    setup videogamesModel.js

---
+++ videogames/videogamesModel.js
@@ -0,0 +1,42 @@
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    findAll,
+    findById,
+    remove,
+    insert,
+    update
+}
+
+function findAll(){
+    return db('videogames')
+}
+
+function findById(id) {
+    return db('videogames')
+    .where({id})
+    .first()
+}
+
+function remove(id) {
+   return db('videogames')
+   .where({id})
+   .del();
+}
+
+function insert(body){
+    return db('videogames')
+    .insert(body, 'id')
+    .then(([id]) => {
+       return findById(id)
+    })
+}
+
+function update (body, id) {
+    return db('videogames')
+    .where({id})
+    .update(body);
+}
+
+
+
